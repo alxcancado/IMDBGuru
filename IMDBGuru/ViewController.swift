ec22c695d9cb9d41999def53c43d6304bf0e7570
alxcancado <alxcancado@gmail.com>
2015-02-22 21:45:49 +1300

    First Commit

---
+++ IMDBGuru/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMDBGuru
+//
+//  Created by alxcancado on 22/02/15.
+//  Copyright (c) 2015 @alxcancado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ IMDBGuruTests/IMDBGuruTests.swift
@@ -0,0 +1,36 @@
+//
+//  IMDBGuruTests.swift
+//  IMDBGuruTests
+//
+//  Created by alxcancado on 22/02/15.
+//  Copyright (c) 2015 @alxcancado. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class IMDBGuruTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
